bbf19e39a57f01588460dd75f357cf60d33be21b
hui.zhao <hui.zhao@ctrip.com>
2017-11-23 21:32:00 +0800

    重构

--- android-godeye/src/main/java/cn/hikyson/godeye/GodEye.java
+++ android-godeye/src/main/java/cn/hikyson/godeye/GodEye.java
@@ -2,6 +2,7 @@ package cn.hikyson.godeye;
 
 import cn.hikyson.godeye.internal.Consumer;
 import cn.hikyson.godeye.internal.Module;
+import cn.hikyson.godeye.internal.engines.Enginee;
 import cn.hikyson.godeye.internal.modules.cpu.Cpu;
 import io.reactivex.Observable;
 
@@ -10,6 +11,17 @@ import io.reactivex.Observable;
  * Created by kysonchao on 2017/11/22.
  */
 public class GodEye {
+    private Cpu mCpu;
+
+    public void cpu(IntervalEnginee enginee){
+        if(mCpu == null){
+            mCpu = new Cpu();
+        }
+
+    }
+
+
+
     public void install() {
         Module<Cpu,Consumer> module = new Module<>(new Cpu(), new Consumer() {
             @Override
