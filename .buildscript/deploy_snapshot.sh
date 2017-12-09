1af058af197b99ca783fd4f6781b2f66b4fcc8a0
hui.zhao <hui.zhao@ctrip.com>
2017-12-09 14:55:38 +0800

    Master deploy 修改为 tag deploy

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -3,7 +3,7 @@
 echo "Deploy snapshot starting..."
 SLUG="Kyson/AndroidGodEye"
 JDK="oraclejdk8"
-BRANCH="master"
+# BRANCH="master"
 
 set -e
 
@@ -13,8 +13,8 @@ elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
   echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Skipping snapshot deployment: was pull request."
-elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
-  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+# elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+#   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 elif [ "$TRAVIS_TAG" == "" ]; then
   echo "Skipping snapshot deployment: no tag found."
 else
