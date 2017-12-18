8a76734ee99133b78daf361b16668f067268cedd
hui.zhao <hui.zhao@ctrip.com>
2017-12-18 20:16:14 +0800

    打tag不会上传snapshot

--- .buildscript/deploy_release.sh
+++ .buildscript/deploy_release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo "[MVN] Deploy release starting >>>>>>>>>>>>>>>>>>>>"
-echo "Make sure VERSION has updated."
 cd .. && ./gradlew clean uploadArchives
 echo "[MVN] Deploy release done. <<<<<<<<<<<<<<<<<<<<"
