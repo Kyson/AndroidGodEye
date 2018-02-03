b679657b2d84e02b7582109057a71ad4d16ebaba
hui.zhao <hui.zhao@ctrip.com>
2018-02-03 14:30:29 +0800

    Leak need permissin

--- android-godeye/src/main/java/cn/hikyson/godeye/core/internal/modules/leakdetector/LeakContextImpl.java
+++ android-godeye/src/main/java/cn/hikyson/godeye/core/internal/modules/leakdetector/LeakContextImpl.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Callable;
 import io.reactivex.Observable;
 
 /**
+ * 权限请求的简单实现，如果有权限则返回true，没有权限返回false，不给外部请求权限的接口了
  * Created by kysonchao on 2017/11/24.
  */
 public class LeakContextImpl implements LeakContext {
@@ -23,11 +24,16 @@ public class LeakContextImpl implements LeakContext {
     }
 
     @Override
-    public Observable<Boolean> permissionNeed(final String permission) {
+    public Observable<Boolean> permissionNeed(final String... permissions) {
         return Observable.fromCallable(new Callable<Boolean>() {
             @Override
             public Boolean call() throws Exception {
-                return PermissionChecker.checkSelfPermission(mApplication, permission) == PermissionChecker.PERMISSION_GRANTED;
+                for (String p : permissions) {
+                    if (PermissionChecker.checkSelfPermission(application(), p) != PermissionChecker.PERMISSION_GRANTED) {
+                        return false;
+                    }
+                }
+                return true;
             }
         });
     }
