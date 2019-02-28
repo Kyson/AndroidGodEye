f7b1dff76b1babd0afa16403a69e967604f7b3d3
hui.zhao <hui.zhao@ctrip.com>
2019-02-28 15:51:55 +0800

    fps显示颜色

--- android-godeye-monitor-dashboard/src/fps/fps.js
+++ android-godeye-monitor-dashboard/src/fps/fps.js
@@ -3,6 +3,7 @@ import '../App.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Row, Col, Clearfix, Grid, Panel} from 'react-bootstrap'
+import Util from '../libs/util'
 
 /**
  * Fps
@@ -13,14 +14,29 @@ class Fps extends Component {
         super(props);
         this.state = {
             fpsInfo: {}
-        }
+        };
+        this.fpsLevelColor = [Util.getGrey(), Util.getRed(), Util.getOrange(), Util.getGreen()];
     }
 
     refresh(fpsInfo) {
         this.setState({fpsInfo});
     }
 
+    static _parseFpsLevel(fpsInfo) {
+        if (fpsInfo && fpsInfo.currentFps && fpsInfo.systemFps) {
+            if (fpsInfo.currentFps >= fpsInfo.systemFps * 5 / 6) {
+                return 3;
+            } else if (fpsInfo.currentFps >= fpsInfo.systemFps / 2) {
+                return 2;
+            } else {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
     render() {
+        let fpsLevel = Fps._parseFpsLevel(this.state.fpsInfo);
         return (
             <Panel style={{textAlign: "left"}}>
                 <Panel.Heading>
@@ -28,7 +44,13 @@ class Fps extends Component {
                     </h5>
                 </Panel.Heading>
                 <Panel.Body>
-                    <h1 style={{textAlign: "center"}}>{(this.state.fpsInfo && this.state.fpsInfo.currentFps && this.state.fpsInfo.systemFps) ? (this.state.fpsInfo.currentFps + "/" + this.state.fpsInfo.systemFps) : "**/**"}</h1>
+                    <h1 style={{
+                        textAlign: "center",
+                        color: this.fpsLevelColor[fpsLevel]
+                    }}>{(this.state.fpsInfo && this.state.fpsInfo.currentFps) ? this.state.fpsInfo.currentFps : "**"}</h1>
+                    <p ><span
+                        style={{fontSize: 8}}>SystemRefreshRate(系统帧率):&nbsp;&nbsp;</span><strong>{(this.state.fpsInfo && this.state.fpsInfo.systemFps) ? this.state.fpsInfo.systemFps : "**"}</strong>
+                    </p>
                 </Panel.Body>
             </Panel>);
     }
