6a487edeaa3f1173c407227a12720fb3dc13d646
hui.zhao <hui.zhao@Ctrip.com>
2019-09-05 18:50:16 +0800

    修改sh

--- android-godeye-idea-plugin/AndroidGodEye_plugin.sh
+++ android-godeye-idea-plugin/AndroidGodEye_plugin.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-adb forward tcp:5390 tcp:5390 && open "http://localhost:5390/index.html" && read -p "Press any key to continue..."
+#! /bin/sh
+
+adb forward tcp:5390 tcp:5390 && open "http://localhost:5390/index.html" && read -p "Press any key"
